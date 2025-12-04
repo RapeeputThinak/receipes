1ea3302e7843f3eed4fafd06c0531e1f40f3bc35
RapeeputThinak <ponrapeeput@gmail.com>
2025-12-05 04:25:18 +0700

    Uniqlo Shirt

--- lib/Model/recipe.dart
+++ lib/Model/recipe.dart
@@ -6,9 +6,20 @@ class Recipe {
 
   static List<Recipe> samples =
   [
-    Recipe('Butternut squash mac and cheese','assets/images/1.webp'),
-    Recipe('Squash and sweet potato soup','assets/images/2.webp'),
-    Recipe('Quince pudding','assets/images/3.webp'),
-    Recipe('Cumin spiced swede and gouda pasties','assets/images/4.webp'),
+    Recipe('CLUB FOR THE FUTURE','assets/images/0.webp'),
+    Recipe('Monsters, Inc','assets/images/1.webp'),
+    Recipe('Mickey Mouse','assets/images/2.webp'),
+    Recipe('Star Wars','assets/images/3.webp'),
+    Recipe('Cinderella','assets/images/4.webp'),
+    Recipe('The Little Mermaid','assets/images/5.webp'),
+    Recipe('Winnie the Pooh','assets/images/6.webp'),
+    Recipe('Chip n Dale','assets/images/7.webp'),
+    Recipe('Dumbo','assets/images/8.webp'),
+    Recipe('Captain America','assets/images/9.webp'),
+    Recipe('Toy Story','assets/images/10.webp'),
+    Recipe('Snow White','assets/images/11.webp'),
+    Recipe('Mickey Mouse','assets/images/12.webp'),
+    Recipe('Disney Art UT','assets/images/13.webp'),
+    Recipe('PEANUTS UT','assets/images/14.webp'),
   ];
 }
